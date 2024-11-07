8daa3728edce477dbf5fe4c42d20061c7e383c54
AdmiRadiance <admirdeok@gmail.com>
2024-11-07 14:31:10 +0100

    Still trying brothers

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import Head from 'next/head';
 import Image from 'next/image';
-import Link from 'next/link';
 import styles from './Home.module.css';
 
 const HomePage = () => {
@@ -12,23 +11,19 @@ const HomePage = () => {
       </Head>
       <div className={styles.container}>
         <div className={styles.imageContainer}>
-          <Link href="https://www.youtube.com/@AdmiRadiance" passHref>
-            <Image
-              className={styles.profileImage}
-              src="/profile.jpg"
-              alt="Profile Picture"
-              width={150}
-              height={150}
-            />
-          </Link>
+          <Image
+            className={styles.profileImage}
+            src="/profile.jpg"
+            alt="Profile Picture"
+            width={150}
+            height={150}
+          />
         </div>
         <h1 className={styles.title}>Welcome to AdmiRadiance!</h1>
         <p className={styles.description}>
           Discover a world of fun and creativity with Roblox! Join us for gameplay, tutorials, and more.
         </p>
-        <Link href="/your-target-page" passHref>
-          <button className={styles.ctaButton}>Get Started</button>
-        </Link>
+        <button className={styles.ctaButton}>Get Started</button>
       </div>
     </>
   );
